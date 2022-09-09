fac25654118550bf0bbc1ccc7756adb09b4703ad
Pravin Se <pvs1516@gmail.comm>
2022-09-09 18:04:31 +0100

    update 23

--- Orchestration with Terraform/main.tf
+++ Orchestration with Terraform/main.tf
@@ -14,7 +14,7 @@ resource "aws_vpc" "pravin_vpc" {
 
 # public subnet
 resource "aws_subnet" "pravin_public" {
-    vpc_id = aws_vpc.pravin_vpc.id   # connecting the to the vpc made in "main" resource
+    vpc_id = aws_vpc.pravin_vpc.id   
     cidr_block = var.pub_cidr_block
 
     tags = {
@@ -24,7 +24,7 @@ resource "aws_subnet" "pravin_public" {
 
 # private subnet
 resource "aws_subnet" "pravin_private" {
-    vpc_id = aws_vpc.pravin_vpc.id   # can also just assign the id of the chosen vpc
+    vpc_id = aws_vpc.pravin_vpc.id   
     cidr_block = var.priv_cidr_block
 
     tags = {
@@ -61,6 +61,51 @@ resource "aws_route_table_association" "pravin_connect_route_to_public" {
     route_table_id = aws_route_table.pravin_public_route.id  
 }
 
+
+# Security Groups
+ resource "aws_security_group" "eng122_pravin_terraform_app_sg"  {
+  name = "eng122_pravin_terraform_app_sg"
+  description = "eng122_pravin_terraform_app_sg"
+  vpc_id = aws_vpc.pravin_vpc.id # attaching the SG with your own VPC
+
+# Inbound rules
+  ingress {
+    from_port       = "80"
+    to_port         = "80"
+    protocol        = "tcp"
+    cidr_blocks     = ["0.0.0.0/0"]   
+  }
+
+# ssh access 
+  ingress {
+    from_port       = "22"
+    to_port         = "22"
+    protocol        = "tcp"
+    cidr_blocks     = ["0.0.0.0/0"]  
+  }
+
+# allow port 3000
+
+ingress {
+    from_port       = "3000"
+    to_port         = "3000"
+    protocol        = "tcp"
+    cidr_blocks     = ["0.0.0.0/0"]  
+  }
+
+# Outbound rules
+  egress {
+    from_port       = 0
+    to_port         = 0
+    protocol        = "-1" # allow all
+    cidr_blocks     = ["0.0.0.0/0"]
+  }
+
+  tags = {
+    Name = "eng122_pravin_terraform_app_sg"
+  }
+}
+
 # ec2
 resource "aws_instance" "eng122_pravin_app_terraform" {
     ami = var.pravin_app_ami
@@ -70,6 +115,8 @@ resource "aws_instance" "eng122_pravin_app_terraform" {
     associate_public_ip_address = true
 
     subnet_id = aws_subnet.pravin_public.id
+
+    vpc_security_group_ids = ["${aws_security_group.eng122_pravin_terraform_app_sg.id}"]
 
     tags = {
       "Name" = "eng122_pravin_app_terraform"
